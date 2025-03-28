e37d2e2b158ed0dd967c99cad0265ba5fd6d20d6
osbaldoXxC <baldo2005_@outlook.com>
2025-03-28 09:19:02 -0600

    pruevba 2

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+const connectDB = require('./config/db');
+const usuarioRoutes = require('./routes/usuarioRoutes');
+
+const app = express();
+
+app.use(cors({
+  origin: [
+    'http://localhost:8081', // Expo Web
+    'exp://192.168.1.4:8081' // Expo Go
+  ]
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+connectDB();
+
+app.use('/api/usuarios', usuarioRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Algo salió mal!' });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Servidor backend corriendo en el puerto ${port}`);
+});
